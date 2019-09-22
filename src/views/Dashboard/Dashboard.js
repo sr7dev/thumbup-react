1623d3097db221a93f6e2fa82b75dbbf18c9364d
JPerfectSolution <j.perfectsolution@outlook.com>
2019-09-22 06:42:33 +0400

    integrting mailgun

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -15,9 +15,51 @@ const mapStateToProps = state => {
 
 const enhance = connect(mapStateToProps);
 
-class Dashboard extends Component {
+class Dashboard extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = { first_name: '', last_name: 'Name', email: 'email@example.com', message: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   componentWillReceiveProps(nextProps) {
     this.reactPageScroller.goToPage(nextProps.pageNo);
+  }
+  
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+
+    const templateId = "template_id";
+
+    this.sendFeedback(
+      templateId,
+      this.state.email,
+      'j.perfectsolution@outlook.com',
+      this.state.message)
+
+    this.setState({
+      formSubmitted: true
+    })
+  }
+  sendFeedback (templateId, senderEmail, receiverEmail, message) {
+    window.emailjs.send(
+      'mailgun',
+      templateId,
+      {
+        senderEmail,
+        receiverEmail,
+        feedback: message
+      })
+      .then(res => {
+        this.setState({ formEmailSent: true })
+      })
+      // Handle errors here however you like, or use a React error boundary
+      .catch(err => console.error('Failed to send feedback. Error: ', err))
   }
 
   loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>;
@@ -231,33 +273,47 @@ class Dashboard extends Component {
                 </Row>
               </Col>
               <Col className="pr-2 pr-sm-3">
-                <Row>
-                  <Col xs="6" className="pr-2 pr-sm-3">
-                    <Input type="text" id="first_name" className="email-addr" required />
-                    <Label htmlFor="first_name">First Name *</Label>
-                  </Col>
-                  <Col xs="6" className="pl-2 pl-sm-3">
-                    <Input type="text" id="last_name" className="email-addr" required />
-                    <Label htmlFor="last_name">Last Name *</Label>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Input type="text" id="email" className="email-addr" required />
-                    <Label htmlFor="email">Email *</Label>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Input type="textarea" id="message" className="email-addr" required />
-                    <Label htmlFor="message">Message *</Label>
-                  </Col>
-                </Row>
-                <div className="text-center mb-60px">
-                  <Button color="dark" outline className="btn-pill join-btn">
-                    Submit
-                  </Button>
-                </div>
+                <form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col xs="6" className="pr-2 pr-sm-3">
+                      <Input type="text" id="first_name" className="email-addr" required 
+                        onChange={handleChange}
+                        name = "first_name"
+                      />
+                      <Label htmlFor="first_name">First Name *</Label>
+                    </Col>
+                    <Col xs="6" className="pl-2 pl-sm-3">
+                      <Input type="text" id="last_name" className="email-addr" required 
+                        onChange={handleChange}
+                        name = "last_name"
+                      />
+                      <Label htmlFor="last_name">Last Name *</Label>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Input type="text" id="email" className="email-addr" required
+                        onChange={handleChange}
+                        name = "email"
+                      />
+                      <Label htmlFor="email">Email *</Label>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Input type="textarea" id="message" className="email-addr" required 
+                        name="message"
+                        onChange={handleChange}
+                      />
+                      <Label htmlFor="message">Message *</Label>
+                    </Col>
+                  </Row>
+                  <div className="text-center mb-60px">
+                    <Button color="dark" outline className="btn-pill join-btn">
+                      Submit
+                    </Button>
+                  </div>
+                </form>
               </Col>
             </Row>
           </Container>
